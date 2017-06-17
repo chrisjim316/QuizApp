b27060580cda95cabb8ba59717601940d3b31870
Chris Jim <chrisjim316@gmail.com>
2017-06-17 17:35:55 +0800

    added animating constraints
    The labels (currentQuestion & nextQuestion )  will now slide on and off the screen.

--- Application/Main/ViewController.swift
+++ Application/Main/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var answerLabel: UILabel!
     
     
@@ -56,6 +58,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         currentQuestionLabel.text = questions[currentQuestionIndex]
+        
+        updateOffScreenLabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -64,15 +68,28 @@ class ViewController: UIViewController {
     }
     
     func animateLabelTransitions() {
-        //animate the alpha
+        
+        // animate the alpha
+        // and the center X constraints
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+
         UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
             self.currentQuestionLabel.alpha = 0
             self.nextQuestionLabel.alpha = 1
+            
+            self.view.layoutIfNeeded()
         },
         completion: {_ in
             swap(&self.currentQuestionLabel,
                  &self.nextQuestionLabel)
+            swap(&self.currentQuestionLabelCenterXConstraint,
+                 &self.nextQuestionLabelCenterXConstraint)
+            
+            self.updateOffScreenLabel()
         })   
+        
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -80,6 +97,11 @@ class ViewController: UIViewController {
         
         //set the label's initial alpha each time this view comes onscreen.
         nextQuestionLabel.alpha = 0
+    }
+    
+    func updateOffScreenLabel() {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
     }
     
 }
